80658a7620182666bac11a336c137e2b485bd4ed
Defn Nghiem <iam@defn.sh>
2017-11-20 19:57:21 -0800

    allow ssh on nat

--- fogg-env/nat.tf
+++ fogg-env/nat.tf
@@ -21,6 +21,15 @@ resource "aws_security_group_rule" "ping_everything" {
   security_group_id        = "${module.nat.network_sg}"
 }
 
+resource "aws_security_group_rule" "forward_allow_ssh" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.env.id}"
+  security_group_id        = "${module.nat.network_sg}"
+}
+
 resource "aws_security_group_rule" "forward_allow_http" {
   type                     = "ingress"
   from_port                = 80

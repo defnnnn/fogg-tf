f02329175a16e73b78840856b454589cb275088f
Defn Nghiem <iam@defn.sh>
2017-11-30 23:57:21 -0800

    define aliases at root

--- fogg-global/.wrapper.tf
+++ fogg-global/.wrapper.tf
@@ -1,33 +1,3 @@
-provider "aws" {
-  alias  = "us_west_2"
-  region = "us-west-2"
-}
-
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
-
-provider "aws" {
-  alias  = "us_east_2"
-  region = "us-east-2"
-}
-
-provider "aws" {
-  alias  = "eu_west_1"
-  region = "eu-west-1"
-}
-
-provider "aws" {
-  alias  = "eu_central_1"
-  region = "eu-central-1"
-}
-
-provider "aws" {
-  alias  = "ap_southeast_2"
-  region = "ap-southeast-2"
-}
-
 module "global" {
   source = "./module/fogg-tf/fogg-global"
 }

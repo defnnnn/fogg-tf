07216613911da8f3b8127adc9215f02bc955f42a
Defn Nghiem <iam@defn.sh>
2017-12-06 00:38:47 -0800

    remote the catchall

--- fogg-api/resource/main.tf
+++ fogg-api/resource/main.tf
@@ -3,15 +3,10 @@ variable "rest_api_id" {}
 variable "resource_id" {}
 variable "invoke_arn" {}
 
-variable "want_vpc_link" {
-  default = 0
-}
-
 variable "http_method" {
   default = "POST"
 }
 
-# /$name
 resource "aws_api_gateway_resource" "fn" {
   rest_api_id = "${var.rest_api_id}"
   parent_id   = "${var.resource_id}"
@@ -25,37 +20,11 @@ resource "aws_api_gateway_method" "fn" {
   authorization = "NONE"
 }
 
-# /$name/{ps+}
-resource "aws_api_gateway_resource" "fn_catch_all" {
-  rest_api_id = "${var.rest_api_id}"
-  parent_id   = "${aws_api_gateway_resource.fn.id}"
-  path_part   = "{ps+}"
-}
-
-resource "aws_api_gateway_method" "fn_catch_all" {
-  rest_api_id   = "${var.rest_api_id}"
-  resource_id   = "${aws_api_gateway_resource.fn_catch_all.id}"
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-# lambda backend
 resource "aws_api_gateway_integration" "fn" {
   rest_api_id             = "${var.rest_api_id}"
   resource_id             = "${aws_api_gateway_resource.fn.id}"
   uri                     = "${replace(var.invoke_arn,"/invocations",":$${stageVariables.alias}/invocations")}"
   http_method             = "${aws_api_gateway_method.fn.http_method}"
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  content_handling        = "CONVERT_TO_TEXT"
-  count                   = "${(var.want_vpc_link - 1)* -1}"
-}
-
-resource "aws_api_gateway_integration" "fn_catch_all" {
-  rest_api_id             = "${var.rest_api_id}"
-  resource_id             = "${aws_api_gateway_resource.fn_catch_all.id}"
-  uri                     = "${replace(var.invoke_arn,"/invocations",":$${stageVariables.alias}/invocations")}"
-  http_method             = "${aws_api_gateway_method.fn_catch_all.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   content_handling        = "CONVERT_TO_TEXT"

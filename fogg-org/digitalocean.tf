d1aa2851019faa0e62474473d00ba2705f2c5417
Defn Nghiem <iam@defn.sh>
2018-01-22 01:55:03 +0000

    private networking

--- fogg-org/digitalocean.tf
+++ fogg-org/digitalocean.tf
@@ -34,15 +34,17 @@ resource "digitalocean_tag" "service" {
 }
 
 resource "digitalocean_droplet" "service" {
-  name       = "${element(var.do_regions,count.index)}${count.index}.${var.domain_name}"
-  ssh_keys   = ["${var.do_ssh_key}"]
-  region     = "${element(var.do_regions,count.index)}"
-  image      = "ubuntu-16-04-x64"
-  size       = "1gb"
-  volume_ids = ["${element(digitalocean_volume.service.*.id,count.index)}"]
-  user_data  = "${data.template_file.user_data_service.rendered}"
-  tags       = ["${digitalocean_tag.org.id}", "${digitalocean_tag.region.id}", "${digitalocean_tag.service.*.id[count.index]}"]
-  count      = "${var.want_digitalocean*var.do_instance_count}"
+  name               = "${element(var.do_regions,count.index)}${count.index}.${var.domain_name}"
+  ssh_keys           = ["${var.do_ssh_key}"]
+  region             = "${element(var.do_regions,count.index)}"
+  image              = "ubuntu-16-04-x64"
+  size               = "1gb"
+  volume_ids         = ["${element(digitalocean_volume.service.*.id,count.index)}"]
+  user_data          = "${data.template_file.user_data_service.rendered}"
+  tags               = ["${digitalocean_tag.org.id}", "${digitalocean_tag.region.id}", "${digitalocean_tag.service.*.id[count.index]}"]
+  ipv6               = true
+  private_networking = true
+  count              = "${var.want_digitalocean*var.do_instance_count}"
 }
 
 resource "digitalocean_firewall" "service" {

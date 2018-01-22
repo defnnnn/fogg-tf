3377f041e14b137ab7bfa1774df43cec811ebe21
Defn Nghiem <iam@defn.sh>
2018-01-22 01:49:59 +0000

    add an org tag

--- fogg-org/digitalocean.tf
+++ fogg-org/digitalocean.tf
@@ -20,6 +20,10 @@ resource "digitalocean_floating_ip" "service" {
   count      = "${var.want_digitalocean*var.do_instance_count}"
 }
 
+resource "digitalocean_tag" "org" {
+  name  = "${var.account_name}"
+}
+
 resource "digitalocean_tag" "service" {
   name  = "${var.account_name}-${element(var.do_regions,count.index)}"
   count = "${var.want_digitalocean*var.do_instance_count}"
@@ -33,7 +37,7 @@ resource "digitalocean_droplet" "service" {
   size       = "1gb"
   volume_ids = ["${element(digitalocean_volume.service.*.id,count.index)}"]
   user_data  = "${data.template_file.user_data_service.rendered}"
-  tags       = ["${digitalocean_tag.service.*.id[count.index]}"]
+  tags       = ["${digitalocean_tag.org.id}", "${digitalocean_tag.service.*.id[count.index]}"]
   count      = "${var.want_digitalocean*var.do_instance_count}"
 }
 

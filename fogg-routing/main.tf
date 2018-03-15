f3f571b9365c8d0ca4a6d7dac6c643c7149c2961
Amanibhavam <iam@defn.sh>
2018-03-15 14:17:31 -0700

    lookup peering connection. cidr from data

--- fogg-routing/main.tf
+++ fogg-routing/main.tf
@@ -1,15 +1,20 @@
 provider "aws" {}
 
-variable "cidr_block" {}
-variable "peer_id" {}
+variable "this_vpc_id" {}
+variable "that_vpc_id" {}
 variable "route_table_id" {}
 
 data "aws_route_table" "rt" {
   route_table_id = "${var.route_table_id}"
 }
 
+data "aws_vpc_peering_connection" "peering" {
+  vpc_id      = "${var.this_vpc_id}"
+  peer_vpc_id = "${var.that_vpc_id}"
+}
+
 resource "aws_route" "rt" {
   route_table_id            = "${data.aws_route_table.rt.id}"
-  destination_cidr_block    = "${var.cidr_block}"
-  vpc_peering_connection_id = "${var.peer_id}"
+  destination_cidr_block    = "${aws_vpc_peering_connection.peering.peer_cidr_block}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.peering.id}"
 }

ed5b5c2de10c0ee0bb8af44a41dc7454d9210426
Amanibhavam <iam@defn.sh>
2018-03-15 18:25:54 -0700

    fix ipv6 routes

--- fogg-env/main.tf
+++ fogg-env/main.tf
@@ -65,7 +65,8 @@ resource "aws_security_group_rule" "allow_zerotier" {
   from_port         = 9993
   to_port           = 9993
   protocol          = "udp"
-  cidr_blocks       = ["0.0.0.0/0", "::/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
   security_group_id = "${aws_security_group.env.id}"
 }
 
@@ -83,7 +84,8 @@ resource "aws_security_group_rule" "env_egress" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0", "::/0"]
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
   security_group_id = "${aws_security_group.env.id}"
 }
 

31ff9d6ea271e8cb2c08c5e9eced9cd299e8dcdb
Amanibhavam <iam@defn.sh>
2018-03-26 18:48:01 -0700

    put back apigateway stuff

--- fogg-org/main.tf
+++ fogg-org/main.tf
@@ -753,3 +753,28 @@ resource "aws_ssm_parameter" "fogg_org" {
   type  = "String"
   value = "${var.account_name}"
 }
+
+resource "aws_iam_role" "api_gateway" {
+  name = "api_gateway_cloudwatch"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+        },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway" {
+  role       = "${aws_iam_role.api_gateway.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}

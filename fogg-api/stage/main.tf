abf3e366f9f7632f7e2ff1aa53d863b7e44400d0
Amanibhavam <iam@defn.sh>
2018-04-25 22:46:16 -0700

    define vars in stages, set desc in deployment

--- fogg-api/stage/main.tf
+++ fogg-api/stage/main.tf
@@ -6,15 +6,16 @@ resource "aws_api_gateway_stage" "stage" {
   stage_name    = "${var.stage_name}"
   rest_api_id   = "${var.rest_api_id}"
   deployment_id = "${aws_api_gateway_deployment.stage.id}"
+
+  variables {
+    alias = "${var.stage_name}"
+  }
 }
 
 resource "aws_api_gateway_deployment" "stage" {
   rest_api_id = "${var.rest_api_id}"
   stage_name  = "${var.stage_name}"
-
-  variables {
-    alias = "${var.stage_name}"
-  }
+  description = "${var.stage_name}"
 
   lifecycle {
     create_before_destroy = true

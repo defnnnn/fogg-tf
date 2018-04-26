9b4e696917a25a9fd5036379c5b8b57815909bed
Amanibhavam <iam@defn.sh>
2018-04-25 22:05:49 -0700

    no idea, this seems to work

--- fogg-api/stage/main.tf
+++ fogg-api/stage/main.tf
@@ -2,6 +2,12 @@ variable "rest_api_id" {}
 variable "stage_name" {}
 variable "domain_name" {}
 
+resource "aws_api_gateway_stage" "stage" {
+  stage_name    = "${var.stage_name}"
+  rest_api_id   = "${var.rest_api_id}"
+  deployment_id = "${aws_api_gateway_deployment.stage.id}"
+}
+
 resource "aws_api_gateway_deployment" "stage" {
   rest_api_id = "${var.rest_api_id}"
   stage_name  = "${var.stage_name}"
@@ -12,7 +18,7 @@ resource "aws_api_gateway_deployment" "stage" {
 }
 
 resource "aws_api_gateway_method_settings" "stage" {
-  depends_on  = ["aws_api_gateway_deployment.stage"]
+  depends_on  = ["aws_api_gateway_stage.stage"]
   rest_api_id = "${var.rest_api_id}"
   stage_name  = "${var.stage_name}"
   method_path = "*/*"
@@ -33,4 +39,12 @@ resource "aws_api_gateway_base_path_mapping" "stage" {
 
 output "deployment" {
   value = "${aws_api_gateway_deployment.stage.id}"
+}
+
+output "invoke_url" {
+  value = "${aws_api_gateway_stage.stage.invoke_url}"
+}
+
+output "execution_arn" {
+  value = "${aws_api_gateway_stage.stage.execution_arn}"
 }

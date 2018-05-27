f5838aae86975cddce0884c3b4ff5fd3448a6bbd
Amanibhavam <iam@defn.sh>
2018-05-27 00:08:32 -0700

    try making this stable

--- fogg-region/main.tf
+++ fogg-region/main.tf
@@ -82,13 +82,13 @@ resource "aws_route53_record" "acm_validation" {
   name    = "${aws_acm_certificate.env.domain_validation_options.0.resource_record_name}"
   type    = "${aws_acm_certificate.env.domain_validation_options.0.resource_record_type}"
   zone_id = "${data.terraform_remote_state.org.public_zone_id}"
-  records = ["${aws_acm_certificate.env.domain_validation_options.0.resource_record_value}"]
+  records = ["${aws_acm_certificate.env.domain_validation_options.0.resource_record_value}."]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "env" {
   certificate_arn         = "${aws_acm_certificate.env.arn}"
-  validation_record_fqdns = ["${aws_route53_record.acm_validation.fqdn}."]
+  validation_record_fqdns = ["${aws_route53_record.acm_validation.fqdn}"]
 }
 
 resource "aws_api_gateway_account" "region" {

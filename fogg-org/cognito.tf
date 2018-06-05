3a2bdfafcef4bb6a9e6f4246890fc3a74a6c20af
Amanibhavam <iam@defn.sh>
2018-06-05 12:15:45 -0700

    new endpoint attribute

--- fogg-org/cognito.tf
+++ fogg-org/cognito.tf
@@ -29,7 +29,7 @@ resource "aws_cognito_identity_pool" "org" {
 
   cognito_identity_providers {
     client_id               = "${aws_cognito_user_pool_client.org.id}"
-    provider_name           = "cognito-idp.${data.aws_region.current.name}.amazonaws.com/${aws_cognito_user_pool.org.id}"
+    provider_name           = "${aws_cognito_user_pool.org.endpoint}"
     server_side_token_check = false
   }
 }
